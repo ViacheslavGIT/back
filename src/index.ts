0dcc4c06db0e0e2361f0605316c38f7589d74951
ViacheslavGIT <gigabazi@gmail.com>
2023-10-09 16:33:35 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.get('/sid', (req, res) => {
+  res.send('Hello !!!!111111!')
+})
+
+app.post('/sid', (req, res) => {
+  res.send('created World!')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
